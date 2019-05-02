66d3261c85348689b67db2ccb799925ad6dbf69a
Moses Holmström <mosesho@crasman.fi>
2019-05-02 11:42:49 +0300

    osa1c

--- osa1/laskuri/src/index.js
+++ osa1/laskuri/src/index.js
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Display = ({counter}) => <div>{counter}</div>;
+
+const Button = ({ handleClick, label}) => (
+  <button onClick={ handleClick }>
+    { label }
+  </button>
+);
+
 const App = (props) => {
   const [ counter, setCounter ] = useState(0);
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const setToValue = (value) => setCounter(value);
+
+  const increment = () => setToValue(counter+1);
+  const decrement = () => setToValue(counter-1);
+  const reset     = () => setToValue(0);
+
   return (
     <div>
-      <h1>{counter}</h1>
+      <Display counter={counter} />
+      <Button handleClick={increment} label="+" />
+      <Button handleClick={decrement} label="-" />
+      <Button handleClick={reset}     label="X" />
     </div>
   );
 }
